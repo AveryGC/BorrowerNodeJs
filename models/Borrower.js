bbd1316a507ee96fb8c083acb095194b1c86935c
Aland <aland.chang@smoothstack.com>
2020-03-03 16:49:22 -0500

    deleted cardNumber property

--- models/Borrower.js
+++ models/Borrower.js
@@ -2,11 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var borrowerSchema = new Schema({
-    cardNumber: {
-        type: Number,
-        required: true,
-        unique: true
-    },
     name: {
         type: String,
         required: true
