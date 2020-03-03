96dae8d56dcf251eb10c76cbcf18fb6ddd80cc0a
AverySSgc <avery.corbett@smoothstack.com>
2020-03-03 17:27:39 -0500

    added endpoints to api

---
+++ controllers/borrowerController.js
@@ -0,0 +1,12 @@
+let routes = require('express').Router();
+let service = require('service\borrowerService.js');
+
+
+
+routes.post('borrowers/:borrowId/branches/:branchId/books/:bookId',function(req,resp){
+    
+})
+
+routes.put('borrowers/:borrowId/branches/:branchId/books/:bookId',function(req,resp){
+
+})
