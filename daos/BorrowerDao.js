f3d75bb9f0adeab55402fb795b45272be79471e3
AverySSgc <avery.corbett@smoothstack.com>
2020-03-10 18:22:48 -0400

    added find by Id for borrower

--- daos/BorrowerDao.js
+++ daos/BorrowerDao.js
@@ -4,6 +4,9 @@ module.exports = {
     find(conditions) {
         return Borrower.find(conditions);
     },
+    findById(id) {
+        return Borrower.findById(id);
+    },
     exists(filter) {
         return Borrower.exists(filter);
     }
